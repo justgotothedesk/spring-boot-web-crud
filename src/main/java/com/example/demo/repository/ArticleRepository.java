3b34709d7119ead2bfcb397382f88f8cc3f38a5f
justgotothedesk <wlscjf3877@naver.com>
2024-01-03 16:15:45 +0900

    Create ArticleRepository interface

---
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Article;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArticleRepository {
+    Article save(Article article);
+    Optional<Article> findById(Long id);
+    Optional<Article> findByTitle(String title);
+    Optional<Article> findByContent(String content);
+    Optional<Article> findByWriter(String writer);
+
+    List<Article> findAll();
+}
