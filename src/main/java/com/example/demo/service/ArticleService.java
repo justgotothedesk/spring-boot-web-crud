5a9754a729446cb65beee6e7dc7eaf80b946c938
justgotothedesk <wlscjf3877@naver.com>
2024-01-03 21:22:54 +0900

    Create Article Service

---
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Article;
+import com.example.demo.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    @Autowired
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public Long create(Article article) {
+        articleRepository.save(article);
+        return article.getId();
+    }
+
+    public List<Article> findArticles() {
+        return articleRepository.findAll();
+    }
+
+    public Optional<Article> findOne(Long id) {
+        return articleRepository.findById(id);
+    }
+}
