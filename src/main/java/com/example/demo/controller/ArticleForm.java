b7b2f774017f19d0e311cf537267e9a3363253c9
justgotothedesk <wlscjf3877@naver.com>
2024-01-04 20:25:07 +0900

    Create ArticleForm class to transfer article's information

---
+++ src/main/java/com/example/demo/controller/ArticleForm.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ArticleForm {
+    private String title;
+    private String content;
+    private String writer;
+}
