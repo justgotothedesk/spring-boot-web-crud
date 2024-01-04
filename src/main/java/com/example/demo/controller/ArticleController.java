624eaabfc29e770e1b3f74219b897665c80364ef
justgotothedesk <wlscjf3877@naver.com>
2024-01-04 20:26:56 +0900

    Add articles and create new article mapping

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Article;
 import com.example.demo.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -11,5 +14,22 @@ public class ArticleController {
     @Autowired
     public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
+    }
+
+    @GetMapping("/articles/new")
+    public String createForm() {
+        return "articles/createArticleForm";
+    }
+
+    @PostMapping("/articles/new")
+    public String create(ArticleForm form) {
+        Article article = new Article();
+        article.setTitle(form.getTitle());
+        article.setContent(form.getContent());
+        article.setWriter(form.getWriter());
+
+        articleService.create(article);
+
+        return "redirect:/";
     }
 }
