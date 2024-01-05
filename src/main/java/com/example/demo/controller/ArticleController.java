660e40e68f755e81c0b0fe0a9b846101d5419e5a
justgotothedesk <wlscjf3877@naver.com>
2024-01-05 17:14:41 +0900

    게시글 목록 화면 요청 처리

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -4,8 +4,13 @@ import com.example.demo.domain.Article;
 import com.example.demo.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ArticleController {
@@ -27,9 +32,38 @@ public class ArticleController {
         article.setTitle(form.getTitle());
         article.setContent(form.getContent());
         article.setWriter(form.getWriter());
-
         articleService.create(article);
-
         return "redirect:/";
+    }
+
+    @GetMapping("articles/")
+    public String list(Model model) {
+        List<Article> articles = articleService.findArticles();
+        model.addAttribute("articles", articles);
+
+        return "articles/articleList";
+    }
+
+    @GetMapping("articles/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        Optional<Article> article = articleService.findOne(id);
+        model.addAttribute("article", article.orElse(null));
+
+        return "articles/articleDetail";
+    }
+
+    @GetMapping("articles/update/{id}")
+    public String updateForm(@PathVariable Long id, Model model) {
+        Article article = articleService.findOne(id).get();
+        model.addAttribute("article", article);
+
+        return "articles/updateArticle";
+    }
+
+    @PostMapping("articles/update/{id}")
+    public String update(@PathVariable Long id, Article newArticle) {
+        articleService.update(id, newArticle);
+
+        return "redirect:/articles";
     }
 }
