f867c6c36987da4e54fa8f986ea8068121f6a9bb
justgotothedesk <wlscjf3877@naver.com>
2024-01-18 14:30:09 +0900

    Session 변수를 전역으로 두어서, 계속 초기화되는 문제점 해결

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,20 +1,24 @@
 package com.example.demo.controller;
+import com.example.demo.domain.Article;
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
     private final UserService userService;
+    private final HttpSession session;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, HttpSession session) {
         this.userService = userService;
+        this.session = session;
     }
 
     @GetMapping("/register")
@@ -31,6 +35,42 @@ public class UserController {
         user.setPassword(form.getPassword());
         userService.create(user);
 
-        return "home";
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String createLoginForm() {
+        return "users/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String id, @RequestParam String password) {
+        Optional<User> userOptional = userService.findByIDAndPassword(id, password);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            session.setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    @GetMapping("/info")
+    public String detail(Model model) {
+        User user = (User) session.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("user", user);
+            return "users/userInfo";
+        } else {
+            return "redirect:/login";
+        }
     }
 }
