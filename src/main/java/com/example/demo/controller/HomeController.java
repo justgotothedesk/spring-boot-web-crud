42ceff5ec16217308264739896cd1ed31133c145
justgotothedesk <wlscjf3877@naver.com>
2024-01-18 14:28:50 +0900

    세션을 통해 로그인 상태인지 로그아웃 상태인지를 전달하는 기능 추가

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("loggedIn", true);
+        } else {
+            model.addAttribute("loggedIn", false);
+        }
+
         return "home";
     }
+
 }
