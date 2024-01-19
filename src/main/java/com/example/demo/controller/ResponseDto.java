efff6006890d3852c60ce8e23e20df31aecd6cba
justgotothedesk <wlscjf3877@naver.com>
2024-01-19 14:27:11 +0900

    ajax 통신 Dto 생성

---
+++ src/main/java/com/example/demo/controller/ResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseDto<T> {
+    private int code;
+    private String msg;
+    private T data;
+}
