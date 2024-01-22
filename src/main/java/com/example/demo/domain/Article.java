470b9a8d6c38249d9dc77c70831e4046726c2871
justgotothedesk <wlscjf3877@naver.com>
2024-01-22 14:24:11 +0900

    Article 테이블에 추천, 비추천 추가

--- src/main/java/com/example/demo/domain/Article.java
+++ src/main/java/com/example/demo/domain/Article.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
@@ -20,11 +22,21 @@ public class Article {
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, updatable = false)
     private User user;
 
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Recog> recommendations;
+
+    private int likeCount;
+    private int dislikeCount;
+
     public Article() {}
-    public Article(Long id, String title, String content, User user) {
+
+    public Article(Long id, String title, String content, User user, List<Recog> recommendations, int likeCount, int dislikeCount) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.user = user;
+        this.recommendations = recommendations;
+        this.likeCount = likeCount;
+        this.dislikeCount = dislikeCount;
     }
 }
